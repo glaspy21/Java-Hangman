b30a7f9960673a9f1c6c54b47a87d7e59544b0d0
Byron Glaspy <byronglaspy@Byrons-MacBook-Pro.local>
2019-10-07 16:34:57 -0400

    initial commit with game, hangman and java classes

---
+++ hangman.java
@@ -0,0 +1,17 @@
+public class Hangman {
+
+  public static void main(String[] args) {
+    // Your incredible code goes here...
+    Game game = new Game("hangman");
+    
+    Prompter prompter = new Prompter(game);
+    
+    while ( game.getRemainingTries() > 0 && !game.isWon()) {
+      prompter.displayProgress();
+      
+      prompter.promptForGuess();
+    }
+    
+    prompter.displayOutcome();
+  }
+}
